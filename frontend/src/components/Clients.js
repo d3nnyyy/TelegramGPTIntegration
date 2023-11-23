8732fa9e35c30ad55cc968531a85e8bb2d44b506
d3nnyyy <d3nnn41k@gmail.com>
2023-11-23 14:38:24 +0200

    implemented Clients component

---
+++ frontend/src/components/Clients.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import ClientProfile from './ClientProfile'
+
+export default function Clients() {
+
+        const [clients, setClients] = useState([])
+
+        useEffect(() => {
+                loadClients()
+        }, [])
+
+        const loadClients = async () => {
+                try {
+
+                        const token = localStorage.getItem('token');
+                        const result = await axios.get('http://localhost:8080/api/clients', {
+                                headers: {
+                                        Authorization: `Bearer ${token}`,
+                                },
+                        });
+
+                        setClients(result.data);
+                } catch (error) {
+                        console.error('Error loading clients:', error);
+                }
+        };
+
+        return (
+                <div className='users-container'>
+                        {clients.map(client => (
+                                <ClientProfile key={client.id} client={client} />
+                        ))}
+                </div>
+        )
+}
