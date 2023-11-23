f452418b711fa61a8b11f8232e4f21ad618d5eaf
d3nnyyy <d3nnn41k@gmail.com>
2023-11-23 11:25:49 +0200

    Initial commit

---
+++ src/main/java/ua/dtsebulia/telegramgptintegration/TelegramGptIntegrationApplication.java
@@ -0,0 +1,13 @@
+package ua.dtsebulia.telegramgptintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramGptIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramGptIntegrationApplication.class, args);
+    }
+
+}
